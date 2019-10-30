f19816111c3b9d169f1eefa1077bd55a8f2c3f00
--Pythonista7 <ashwin.mirskar@gmail.com>
2019-10-30 08:48:54 +0530

    akjsdb

--- NYCJobs/settings.py
+++ NYCJobs/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0)*!&#bbb)4ci-ur8!j#x#)vr8d(0r5-%%jqs5sl7bt=l6jsfy'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['binaryblueapp.herokuapp.com']
 
